40e0e087d4f0cb9daeb129dcd57af3ffcfbf4d5b
Shameel Abdullah <shameel.abdullah@gmail.com>
2020-05-18 20:56:28 -0400

    Add support for viewing from feed/profile

--- src/index.js
+++ src/index.js
@@ -1,33 +1,67 @@
-const video = document.getElementsByTagName("video")[0];
-
-const userInfo = document.getElementsByClassName("user-info")[0];
-
-const wrapperDiv = document.createElement("div");
-const percentageSpan = document.createElement("span");
+const URL = window.location.toString();
 let percentage = 10;
-video.volume = percentage / 100;
-percentageSpan.textContent = `${percentage}%`;
 
-let increaseButton = document.createElement("button");
-increaseButton.innerHTML = "+";
-increaseButton.addEventListener("click", () => {
-  if (percentage == 100) return;
-  percentage += 5;
-  percentageSpan.textContent = `${percentage}%`;
+function increaseButtonHTML(video, percentageSpan) {
+  let increaseButton = document.createElement("button");
+  increaseButton.innerHTML = "+";
+  increaseButton.addEventListener("click", () => {
+    if (percentage == 100) return;
+    percentage += 2;
+    percentageSpan.textContent = `${percentage}%`;
+    video.volume = percentage / 100;
+  });
+  return increaseButton;
+}
+
+function decreaseButtonHTML(video, percentageSpan) {
+  let decreaseButton = document.createElement("button");
+  decreaseButton.innerHTML = "-";
+  decreaseButton.addEventListener("click", () => {
+    if (percentage == 0) return;
+    percentage -= 2;
+    percentageSpan.textContent = `${percentage}%`;
+    video.volume = percentage / 100;
+  });
+  return decreaseButton;
+}
+
+function generateInjectedHTML(video) {
   video.volume = percentage / 100;
-});
-
-let decreaseButton = document.createElement("button");
-decreaseButton.innerHTML = "-";
-decreaseButton.addEventListener("click", () => {
-  if (percentage == 0) return;
-  percentage -= 5;
+  const userInfo = document.getElementsByClassName("user-info")[0];
+  const percentageSpan = document.createElement("span");
   percentageSpan.textContent = `${percentage}%`;
-  video.volume = percentage / 100;
-});
+  const increaseButton = increaseButtonHTML(video, percentageSpan);
+  const decreaseButton = decreaseButtonHTML(video, percentageSpan);
+  const wrapperDiv = document.createElement("div");
+  const volumeLabel = document.createElement("span");
+  volumeLabel.innerHTML = "VOLUME: ";
+  wrapperDiv.append(
+    volumeLabel,
+    decreaseButton,
+    percentageSpan,
+    increaseButton
+  );
+  userInfo.after(wrapperDiv);
+}
 
-const volumeLabel = document.createElement("span");
-volumeLabel.innerHTML = "VOLUME: ";
-
-wrapperDiv.append(volumeLabel, decreaseButton, percentageSpan, increaseButton);
-userInfo.after(wrapperDiv);
+// On individual video page
+if (URL.includes("/video/")) {
+  const video = document.getElementsByTagName("video")[0];
+  generateInjectedHTML(video);
+} else {
+  const clickFunction = () => {
+    setTimeout(() => {
+      const vid = document.getElementsByClassName("horizontal")[0];
+      generateInjectedHTML(vid);
+    }, 0);
+  };
+  window.onload = function() {
+    const clickables = document.getElementsByClassName(
+      "video-feed-item-wrapper"
+    );
+    for (var i = 0; i < clickables.length; i++) {
+      const video = clickables[i];
+      video.addEventListener("click", clickFunction);
+    }
+  };
+}
