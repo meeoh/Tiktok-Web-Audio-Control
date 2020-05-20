66410b1d6de801c508b00ccaa22be20e96413255
Shameel Abdullah <shameel.abdullah@gmail.com>
2020-05-19 20:30:16 -0400

    Rerun on url change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const URL = window.location.toString();
+const URL = window.location.toString().toLowerCase();
 let percentage = 10;
 let adjustRate = 5;
+let hasTrendingRan = false;
 
 function toArray(obj) {
   var array = [];
@@ -55,12 +56,43 @@ function generateInjectedHTML(video) {
   userInfo.after(wrapperDiv);
 }
 
-const clickFunction = () => {
+function clickFunction() {
   setTimeout(() => {
     const vid = document.getElementsByClassName("video-player horizontal")[0];
     generateInjectedHTML(vid);
   }, 0);
-};
+}
+
+function trending() {
+  hasTrendingRan = true;
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (!mutation.addedNodes) return;
+
+      for (var i = 0; i < mutation.addedNodes.length; i++) {
+        // do things to your newly added nodes here
+        var node = mutation.addedNodes[i];
+        if (toArray(node.classList).indexOf("video-feed-item") > -1) {
+          node.addEventListener("click", () => clickFunction(node));
+        }
+      }
+    });
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: false,
+    characterData: false
+  });
+}
+
+function attachListenerToClickables(className) {
+  const clickables = toArray(document.getElementsByClassName(className));
+  clickables.forEach(video => {
+    video.addEventListener("click", clickFunction);
+  });
+}
 
 chrome.storage.sync.get("adjustRate", function(result) {
   if (result && result.adjustRate) {
@@ -70,37 +102,21 @@ chrome.storage.sync.get("adjustRate", function(result) {
   if (URL.includes("/video/")) {
     const video = document.getElementsByTagName("video")[0];
     generateInjectedHTML(video);
+  } else if (URL.includes("@")) {
+    window.onload = function() {
+      attachListenerToClickables("video-feed-item");
+    };
+  } else if (URL.includes("trending")) {
+    trending();
+  } else if (URL.includes("discover")) {
+    attachListenerToClickables("video-card");
+  }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.title.includes("Trending") && !hasTrendingRan) {
+    trending();
   } else {
-    if (URL.includes("@")) {
-      window.onload = function() {
-        const clickables = toArray(
-          document.getElementsByClassName("video-feed-item")
-        );
-        clickables.forEach(video => {
-          video.addEventListener("click", clickFunction);
-        });
-      };
-    } else {
-      var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-          if (!mutation.addedNodes) return;
-
-          for (var i = 0; i < mutation.addedNodes.length; i++) {
-            // do things to your newly added nodes here
-            var node = mutation.addedNodes[i];
-            if (toArray(node.classList).indexOf("video-feed-item") > -1) {
-              node.addEventListener("click", () => clickFunction(node));
-            }
-          }
-        });
-      });
-
-      observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-        attributes: false,
-        characterData: false
-      });
-    }
+    attachListenerToClickables("video-card");
   }
 });
